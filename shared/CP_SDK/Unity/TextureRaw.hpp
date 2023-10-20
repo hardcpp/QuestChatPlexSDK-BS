ae868e854923680261bc8c5d1ffde39983439445
HardCPP <hardcpp@gmail.com>
2023-10-20 03:33:59 +0200

    Fix texture raw memory management

--- shared/CP_SDK/Unity/TextureRaw.hpp
+++ shared/CP_SDK/Unity/TextureRaw.hpp
@@ -19,12 +19,15 @@ namespace CP_SDK::Unity {
         DISABLE_CONSTRUCTORS(TextureRaw);
 
         public:
+            using PixelArray = std::shared_ptr<std::vector<_u::Color>>;
+
+        public:
             /// @brief Load an image into raw pixels
             /// @param p_Bytes  Input bytes
             /// @param p_Width  Ouput width
             /// @param p_Height Output height
             /// @param p_Pixels Output pixels
-            static bool Load(::Array<uint8_t>* p_Bytes, int& p_Width, int& p_Height, std::vector<_u::Color>& p_Pixels);
+            static bool Load(::Array<uint8_t>* p_Bytes, int& p_Width, int& p_Height, PixelArray* p_Pixels);
 
         public:
             /// @brief Blur an image
@@ -32,11 +35,11 @@ namespace CP_SDK::Unity {
             /// @param p_InHeight Source height
             /// @param p_InPixels Input image
             /// @param p_Radius   Blur radius
-            static void FastGaussianBlur(int p_InWidth, int p_InHeight, std::vector<_u::Color>& p_InPixels, int p_Radius);
+            static void FastGaussianBlur(int p_InWidth, int p_InHeight, PixelArray& p_InPixels, int p_Radius);
             /// @brief Multiply image A & B into A
             /// @param p_ImageA Target
             /// @param p_ImageB Additional image
-            static void Multiply(std::vector<_u::Color>& p_ImageA, std::vector<_u::Color>& p_ImageB);
+            static void Multiply(PixelArray& p_ImageA, PixelArray& p_ImageB);
             /// @brief Resize an image and crop it
             /// @param p_InWidth      Source width
             /// @param p_InHeight     Source height
@@ -44,14 +47,14 @@ namespace CP_SDK::Unity {
             /// @param p_TargetWidth  Target width
             /// @param p_TargetHeight Target height
             /// @param p_YOffsetRel   Height anchor
-            static std::vector<_u::Color> ResampleAndCrop(int p_InWidth, int p_InHeight, std::vector<_u::Color>& p_InPixels, int p_TargetWidth, int p_TargetHeight, float p_YOffsetRel = 0.5f);
+            static PixelArray ResampleAndCrop(int p_InWidth, int p_InHeight, PixelArray& p_InPixels, int p_TargetWidth, int p_TargetHeight, float p_YOffsetRel = 0.5f);
 
         private:
             static std::vector<int> BoxesForGauss(int p_Radius, int p_Count);
-            static void GaussianBlur4(int p_Width, int p_Height, std::vector<_u::Color>& p_InPixels, int p_Radius);
-            static void BoxBlur4 (int p_Width, int p_Height, std::vector<_u::Color>& p_InPixels, int p_Radius);
-            static void BoxBlurH4(int p_Width, int p_Height, std::vector<_u::Color>& p_InPixels, int p_Radius);
-            static void BoxBlurT4(int p_Width, int p_Height, std::vector<_u::Color>& p_InPixels, int p_Radius);
+            static void GaussianBlur4(int p_Width, int p_Height, PixelArray& p_InPixels, int p_Radius);
+            static void BoxBlur4 (int p_Width, int p_Height, PixelArray& p_InPixels, int p_Radius);
+            static void BoxBlurH4(int p_Width, int p_Height, PixelArray& p_InPixels, int p_Radius);
+            static void BoxBlurT4(int p_Width, int p_Height, PixelArray& p_InPixels, int p_Radius);
 
     };
 
--- src/CP_SDK/Unity/TextureRaw.cpp
+++ src/CP_SDK/Unity/TextureRaw.cpp
@@ -19,8 +19,13 @@ namespace CP_SDK::Unity {
     /// @param p_Width  Ouput width
     /// @param p_Height Output height
     /// @param p_Pixels Output pixels
-    bool TextureRaw::Load(::Array<uint8_t>* p_Bytes, int& p_Width, int& p_Height, std::vector<Color>& p_Pixels)
+    bool TextureRaw::Load(::Array<uint8_t>* p_Bytes, int& p_Width, int& p_Height, PixelArray* p_Pixels)
     {
+        if (!p_Pixels)
+            return false;
+
+        *p_Pixels = nullptr;
+
         p_Width     = 0;
         p_Height    = 0;
 
@@ -38,12 +43,15 @@ namespace CP_SDK::Unity {
         if (!l_STBIBuffer)
             return false;
 
-        p_Pixels.resize(p_Width * p_Height);
+        *p_Pixels = std::make_shared<std::vector<Color>>();
+
+        auto& l_Pixels = *(*p_Pixels).get();
+        l_Pixels.resize(p_Width * p_Height);
 
         for (auto l_I = 0; l_I < (p_Width * p_Height); ++l_I)
         {
             auto l_SrcPixel = &l_STBIBuffer[l_I * 4];
-            p_Pixels[l_I] = Extensions::ColorU::Convert(Color32(l_SrcPixel[0], l_SrcPixel[1], l_SrcPixel[2], l_SrcPixel[3]));
+            l_Pixels[l_I] = Extensions::ColorU::Convert(Color32(l_SrcPixel[0], l_SrcPixel[1], l_SrcPixel[2], l_SrcPixel[3]));
         }
 
         stbi_image_free(l_STBIBuffer);
@@ -59,20 +67,23 @@ namespace CP_SDK::Unity {
     /// @param p_InHeight Source height
     /// @param p_InPixels Input image
     /// @param p_Radius   Blur radius
-    void TextureRaw::FastGaussianBlur(int p_InWidth, int p_InHeight, std::vector<Color>& p_InPixels, int p_Radius)
+    void TextureRaw::FastGaussianBlur(int p_InWidth, int p_InHeight, PixelArray& p_InPixels, int p_Radius)
     {
         GaussianBlur4(p_InWidth, p_InHeight, p_InPixels, p_Radius);
     }
     /// @brief Multiply image A & B into A
     /// @param p_ImageA Target
     /// @param p_ImageB Additional image
-    void TextureRaw::Multiply(std::vector<Color>& p_ImageA, std::vector<Color>& p_ImageB)
+    void TextureRaw::Multiply(PixelArray& p_ImageA, PixelArray& p_ImageB)
     {
-        if (p_ImageA.size() != p_ImageB.size())
-            throw std::runtime_error("[CP_SDK.Unity][TextureRaw.Multiply] Size differ! " + std::to_string(p_ImageA.size()) + " vs " + std::to_string(p_ImageB.size()));
+        if (p_ImageA->size() != p_ImageB->size())
+            throw std::runtime_error("[CP_SDK.Unity][TextureRaw.Multiply] Size differ! " + std::to_string(p_ImageA->size()) + " vs " + std::to_string(p_ImageB->size()));
 
-        for (auto l_I = 0; l_I < p_ImageA.size(); ++l_I)
-            p_ImageA[l_I] = p_ImageA[l_I] * p_ImageB[l_I];
+        auto& l_ImageA = *(p_ImageA.get());
+        auto& l_ImageB = *(p_ImageB.get());
+
+        for (auto l_I = 0; l_I < p_ImageA->size(); ++l_I)
+            l_ImageA[l_I] = l_ImageA[l_I] * l_ImageB[l_I];
     }
     /// @brief Resize an image and crop it
     /// @param p_InWidth      Source width
@@ -81,7 +92,7 @@ namespace CP_SDK::Unity {
     /// @param p_TargetWidth  Target width
     /// @param p_TargetHeight Target height
     /// @param p_YOffsetRel   Height anchor
-    std::vector<Color> TextureRaw::ResampleAndCrop(int p_InWidth, int p_InHeight, std::vector<Color>& p_InPixels, int p_TargetWidth, int p_TargetHeight, float p_YOffsetRel)
+    TextureRaw::PixelArray TextureRaw::ResampleAndCrop(int p_InWidth, int p_InHeight, PixelArray& p_InPixels, int p_TargetWidth, int p_TargetHeight, float p_YOffsetRel)
     {
         float l_SourceAspect = (float)p_InWidth / p_InHeight;
         float l_TargetAspect = (float)p_TargetWidth / p_TargetHeight;
@@ -105,7 +116,10 @@ namespace CP_SDK::Unity {
             l_YOffset = (int)((p_InHeight - p_InWidth / l_TargetAspect) * (1.0f - p_YOffsetRel));
         }
 
-        std::vector<Color> l_Result;
+        auto  l_ResultPtr   = std::make_shared<std::vector<Color>>();
+        auto& l_Result      = *(l_ResultPtr.get());
+        auto& l_InPixels    = *(p_InPixels.get());
+
         l_Result.resize(p_TargetWidth * p_TargetHeight);
 
         for (int l_Y = 0; l_Y < p_TargetHeight; ++l_Y)
@@ -121,16 +135,16 @@ namespace CP_SDK::Unity {
                 auto l_CY = Mathf::CeilToInt(l_Pixel.y);
 
                 /// Bilinear filtering
-                auto l_C11 = p_InPixels[l_FX + p_InWidth * l_FY];
-                auto l_C12 = p_InPixels[l_FX + p_InWidth * l_CY];
-                auto l_C21 = p_InPixels[l_CX + p_InWidth * l_FY];
-                auto l_C22 = p_InPixels[l_CX + p_InWidth * l_CY];
+                auto l_C11 = l_InPixels[l_FX + p_InWidth * l_FY];
+                auto l_C12 = l_InPixels[l_FX + p_InWidth * l_CY];
+                auto l_C21 = l_InPixels[l_CX + p_InWidth * l_FY];
+                auto l_C22 = l_InPixels[l_CX + p_InWidth * l_CY];
 
                 l_Result[l_X + l_Y * p_TargetWidth] = Color::Lerp(Color::Lerp(l_C11, l_C12, l_Pixel.y), Color::Lerp(l_C21, l_C22, l_Pixel.y), l_Pixel.x);
             }
         }
 
-        return l_Result;
+        return l_ResultPtr;
     }
 
     ////////////////////////////////////////////////////////////////////////////
@@ -155,79 +169,81 @@ namespace CP_SDK::Unity {
 
         return l_Sizes;
     }
-    void TextureRaw::GaussianBlur4(int p_Width, int p_Height, std::vector<Color>& p_InPixels, int p_Radius)
+    void TextureRaw::GaussianBlur4(int p_Width, int p_Height, PixelArray& p_InPixels, int p_Radius)
     {
         auto l_Boxes = BoxesForGauss(p_Radius, 3);
         BoxBlur4(p_Width, p_Height, p_InPixels, (l_Boxes[0] - 1) / 2);
         BoxBlur4(p_Width, p_Height, p_InPixels, (l_Boxes[1] - 1) / 2);
         BoxBlur4(p_Width, p_Height, p_InPixels, (l_Boxes[2] - 1) / 2);
     }
-    void TextureRaw::BoxBlur4(int p_Width, int p_Height, std::vector<Color>& p_InPixels, int p_Radius)
+    void TextureRaw::BoxBlur4(int p_Width, int p_Height, PixelArray& p_InPixels, int p_Radius)
     {
         BoxBlurH4(p_Width, p_Height, p_InPixels, p_Radius);
         BoxBlurT4(p_Width, p_Height, p_InPixels, p_Radius);
     }
-    void TextureRaw::BoxBlurH4(int p_Width, int p_Height, std::vector<Color>& p_InPixels, int p_Radius)
+    void TextureRaw::BoxBlurH4(int p_Width, int p_Height, PixelArray& p_InPixels, int p_Radius)
     {
-        auto l_Mult = (float)1 / (p_Radius + p_Radius + 1);
+        auto& l_InPixels    = *(p_InPixels.get());
+        auto  l_Mult        = (float)1 / (p_Radius + p_Radius + 1);
         for (auto l_I = 0; l_I < p_Height; ++l_I)
         {
             auto l_TI  = l_I * p_Width;
             auto l_LI  = l_TI;
             auto l_RI  = l_TI + p_Radius;
-            auto l_FV  = p_InPixels[l_TI];
-            auto l_LV  = p_InPixels[l_TI + p_Width - 1];
+            auto l_FV  = l_InPixels[l_TI];
+            auto l_LV  = l_InPixels[l_TI + p_Width - 1];
             auto l_Val = (p_Radius + 1) * l_FV;
 
             for (auto l_J = 0; l_J < p_Radius; ++l_J)
-                l_Val = l_Val + (p_InPixels[l_TI + l_J]);
+                l_Val = l_Val + (l_InPixels[l_TI + l_J]);
 
             for (auto l_J = 0; l_J <= p_Radius; ++l_J)
             {
-                l_Val = l_Val + (p_InPixels[l_RI++] - l_FV);
-                p_InPixels[l_TI++] = l_Val * l_Mult;
+                l_Val = l_Val + (l_InPixels[l_RI++] - l_FV);
+                l_InPixels[l_TI++] = l_Val * l_Mult;
             }
 
             for (auto l_J = p_Radius + 1; l_J < p_Width - p_Radius; ++l_J)
             {
-                l_Val = l_Val + (p_InPixels[l_RI++] - p_InPixels[l_LI++]);
-                p_InPixels[l_TI++] = l_Val * l_Mult;
+                l_Val = l_Val + (l_InPixels[l_RI++] - l_InPixels[l_LI++]);
+                l_InPixels[l_TI++] = l_Val * l_Mult;
             }
 
             for (auto l_J = p_Width - p_Radius; l_J < p_Width; ++l_J)
             {
-                l_Val = l_Val + (l_LV - p_InPixels[l_LI++]);
-                p_InPixels[l_TI++] = l_Val * l_Mult;
+                l_Val = l_Val + (l_LV - l_InPixels[l_LI++]);
+                l_InPixels[l_TI++] = l_Val * l_Mult;
             }
         };
     }
-    void TextureRaw::BoxBlurT4(int p_Width, int p_Height, std::vector<Color>& p_InPixels, int p_Radius)
+    void TextureRaw::BoxBlurT4(int p_Width, int p_Height, PixelArray& p_InPixels, int p_Radius)
     {
-        auto l_Mult = (float)1 / (p_Radius + p_Radius + 1);
+        auto& l_InPixels    = *(p_InPixels.get());
+        auto  l_Mult        = (float)1 / (p_Radius + p_Radius + 1);
         for (auto l_I = 0; l_I < p_Width; l_I++)
         {
             auto l_TI  = l_I;
             auto l_LI  = l_TI;
             auto l_RI  = l_TI + p_Radius * p_Width;
-            auto l_FV  = p_InPixels[l_TI];
-            auto l_LV  = p_InPixels[l_TI + p_Width * (p_Height - 1)];
+            auto l_FV  = l_InPixels[l_TI];
+            auto l_LV  = l_InPixels[l_TI + p_Width * (p_Height - 1)];
             auto l_Val = (p_Radius + 1) * l_FV;
 
             for (auto l_J = 0; l_J < p_Radius; ++l_J)
-                l_Val = l_Val + (p_InPixels[l_TI + l_J * p_Width]);
+                l_Val = l_Val + (l_InPixels[l_TI + l_J * p_Width]);
 
             for (auto l_J = 0; l_J <= p_Radius; ++l_J)
             {
-                l_Val = l_Val + (p_InPixels[l_RI] - l_FV);
-                p_InPixels[l_TI] = l_Val * l_Mult;
+                l_Val = l_Val + (l_InPixels[l_RI] - l_FV);
+                l_InPixels[l_TI] = l_Val * l_Mult;
                 l_RI += p_Width;
                 l_TI += p_Width;
             }
 
             for (auto l_J = p_Radius + 1; l_J < p_Height - p_Radius; ++l_J)
             {
-                l_Val = l_Val + (p_InPixels[l_RI] - p_InPixels[l_LI]);
-                p_InPixels[l_TI] = l_Val * l_Mult;
+                l_Val = l_Val + (l_InPixels[l_RI] - l_InPixels[l_LI]);
+                l_InPixels[l_TI] = l_Val * l_Mult;
                 l_LI += p_Width;
                 l_RI += p_Width;
                 l_TI += p_Width;
@@ -235,8 +251,8 @@ namespace CP_SDK::Unity {
 
             for (auto l_J = p_Height - p_Radius; l_J < p_Height; ++l_J)
             {
-                l_Val = l_Val + (l_LV - p_InPixels[l_LI]);
-                p_InPixels[l_TI] = l_Val * l_Mult;
+                l_Val = l_Val + (l_LV - l_InPixels[l_LI]);
+                l_InPixels[l_TI] = l_Val * l_Mult;
                 l_LI += p_Width;
                 l_TI += p_Width;
             }
