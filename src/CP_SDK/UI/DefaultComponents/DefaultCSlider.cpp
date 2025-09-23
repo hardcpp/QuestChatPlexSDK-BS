6794b4dca75a3f2e387b4b6f5a58c1715903ad3d
HardCPP <hardcpp@gmail.com>
2025-09-23 22:10:26 +0200

    UI/XUI: Fix button text/background color + slider styling

--- src/CP_SDK/UI/DefaultComponents/DefaultCSlider.cpp
+++ src/CP_SDK/UI/DefaultComponents/DefaultCSlider.cpp
@@ -219,8 +219,8 @@ namespace CP_SDK::UI::DefaultComponents {
         m_OnColor   = p_Color;
         m_OffColor  = p_Color;
 
-        m_DecButton->SetColor(p_Color);
-        m_IncButton->SetColor(p_Color);
+        m_DecButton->SetBackgroundColor(p_Color);
+        m_IncButton->SetBackgroundColor(p_Color);
 
         UpdateStyle();
     }
@@ -537,8 +537,8 @@ namespace CP_SDK::UI::DefaultComponents {
     void DefaultCSlider::UpdateStyle()
     {
         auto l_IsInteractable = IsInteractable();
-        m_DecButton->SetColor(l_IsInteractable ? m_OnColor : m_OffColor);
-        m_IncButton->SetColor(l_IsInteractable ? m_OnColor : m_OffColor);
+        m_DecButton->SetBackgroundColor(l_IsInteractable ? m_OnColor : m_OffColor);
+        m_IncButton->SetBackgroundColor(l_IsInteractable ? m_OnColor : m_OffColor);
 
         if (m_LeftMouseButtonPressed || m_LastPointerEvent)
         {
